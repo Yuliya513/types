f8e6bbc02a570cc7b5a8974f74641e8900400d44
Yuliya513 <yuliya.senik@gmail.com>
2021-05-14 15:03:19 +0300

    types with break

---
+++ src/com/company/types.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class types {
+
+    public static void main(String[] args) {
+	private static String name Dog=bull;
+        private static String name Dog=peekenes;
+        private static String name Dog=chaochao;
+        public static void main (String args [])
+        {
+            int age=10;
+            switch (dog){
+                case dog;
+                System.out.printl ("Is it a bull");
+                break;
+                System.out.printl ("Is it a chaochao");
+                break;
+                System.out.printl ("Is it a peekenes");
+                break;
+                default
+            }
+        }
+    }
+}
